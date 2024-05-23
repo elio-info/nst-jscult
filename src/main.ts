ea1924f9a680fd7ab0940f5ce73bda7b91c5d39b
elio-info <dario.andreu@gmail.com>
2024-05-23 19:21:07 -0400

    configurando documentacion

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //configurando tiutlos de documentacion
+  let opciones=new DocumentBuilder()
+    .setTitle('Esta es la documentacion')
+    .setDescription(' esta es la API de prueba')
+    .setVersion('0.1')
+    .build()
+  let documentSwgg= SwaggerModule.createDocument(app,opciones)
+    SwaggerModule.setup('docs',app,documentSwgg)
+  //fin configurando tiutlos de documentacion
   await app.listen(3000);
 }
 bootstrap();
