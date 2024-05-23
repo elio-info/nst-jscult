0d4e2f2f6ca58ff58a3db7fc71b45ba2011673bb
elio-info <dario.andreu@gmail.com>
2024-05-23 19:52:12 -0400

    una conexion a MOngo

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,13 @@ import { NomencladorEtarioClssController } from './nomenclador_etario_clss/nomen
 import { NomencladorEtarioClssService } from './nomenclador_etario_clss/nomenclador_etario_clss.service';
 import { NomencladorEtarioClssModule } from './nomenclador_etario_clss/nomenclador_etario_clss.module';
 import { NomencladorEstadosactividadClssModule } from './nomenclador_estadosactividad_clss/nomenclador_estadosactividad_clss.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [NomencladorEtarioClssModule, NomencladorEstadosactividadClssModule],
+  imports: [NomencladorEtarioClssModule, NomencladorEstadosactividadClssModule,
+    //importando Mongoose
+    MongooseModule.forRoot('mongodb://localhost:27017/nomenclador_estadosactividad')
+  ],
   controllers: [AppController,NomencladorEtarioClssController],
   providers: [AppService, NomencladorEtarioClssService],
 })
