0d132d1d220b224f2c2f7b2409b1a2d201360605
Data Mining <dario.andreu@gmail.com>
2024-06-03 01:44:06 -0400

    creando tabla Nomenclador Categoria Contratacion manifestacion

--- src/nomencla_categorias-contratacion-manifestacion/dto/create-nomencla_categorias-contratacion-manifestacion.dto.ts
+++ src/nomencla_categorias-contratacion-manifestacion/dto/create-nomencla_categorias-contratacion-manifestacion.dto.ts
@@ -1,1 +1,19 @@
-export class Create_Nomencla_CategoriasContratacionManifestacion_Dto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsBoolean, IsNotEmpty, IsString } from "class-validator"
+
+export class Create_Nomencla_CategoriasContratacionManifestacion_Dto {
+    @ApiProperty({
+        example:'Nombre del Nomenclador. Ej: música, danza, audio, transporte, luces, etc.'
+    })
+    @IsNotEmpty({message:'El tipo de categoria no puede ser vacio.'})
+    @IsString({message:'El nomenclador no puede ser numeral o caracter especial'})
+    readonly nombre_categoria_manifestacion :string
+
+    @ApiProperty({
+        example:'Este campo es verdadero por defecto'
+    })
+    @IsBoolean({
+        message:'Solo Si o No'
+    })
+    apoyo_categoria_manifestacion:boolean
+}
